17043ec9cbda117578e31ea418866b720252b93e
R123ay <163374090+R123ay@users.noreply.github.com>
2024-06-13 16:33:46 +0800

    customer改進中

--- src/main/java/com/chocoshop/model/dto/CustomerDto.java
+++ src/main/java/com/chocoshop/model/dto/CustomerDto.java
@@ -3,26 +3,24 @@ package com.chocoshop.model.dto;
 import java.util.List;
 
 import org.springframework.stereotype.Repository;
+
 import com.chocoshop.model.po.Customer;
 
 @Repository
 public interface CustomerDto {
     
     // Retrieve all customers
-    List<Customer> findAll(); // To get all customers
-    
+    List<Customer> findAll();
+
     // Retrieve a customer by their ID
-    Customer findById(Integer id); // To get a customer by their ID
-    
+    Customer findById(Integer id);
+
     // Add a new customer
-    Integer add(Customer customer); // To add a new customer
-    
+    Integer add(Customer customer);
+
     // Delete a customer by their ID
-    Integer deleteById(Integer id); // To delete a customer by their ID
-    
-    // Update customer information
-    Integer update(Customer customer); // To update customer information
-    
+    Integer deleteById(Integer id);
+
     // Edit customer information
-    Customer edit(Integer id, Customer customer); // To edit customer information
+    Customer edit(Integer id, Customer customer);
 }
